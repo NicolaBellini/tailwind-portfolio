a1cb46d615a8b5184c0ce3ecba37d25a9ccdbd3e
NicolaBellini <Bellininicola4366@gmail.com>
2024-07-23 15:26:23 +0200

    scafolding

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+import Header from "./components/header.vue";
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
+
+<template>
+  <Header />
+</template>
+
+<style lang="scss" scoped></style>
